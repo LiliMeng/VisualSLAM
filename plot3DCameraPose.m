e5abc82c6f1f193e61b43e0687ab1278910f53e0
LiliMeng <lilimeng1103@gmail.com>
2015-11-06 18:22:27 -0800

    Update plot3DCameraPose.m

--- plot3DCameraPose.m
+++ plot3DCameraPose.m
@@ -1,8 +1,16 @@
 function plot3DCameraPose(x,y,z)
-test = xlsread('camera_pose.xlsx', 'A2:C1665');
+figure
+%test = xlsread('camera_pose.xlsx', 'A2:C1665');
+test = xlsread('camera_pose_Keyframe_MotionMetric0.1.xlsx', 'A1:C195');
 x=test(:,1);
 y=test(:,2);
 z=test(:,3);
-figure
-plot3(x,y,z)
+
+%plot3(x,y,z);
+title('Raw Camera Pose from Keyframe Motion Metric Threshold 0.1');
+scatter3(x,y,z,'filled')
+xlabel('x position (meter)'); % x-axis label
+ylabel('y position (meter)');% y-axis label
+zlabel('z position (meter)');% z-axis label
+
 end
